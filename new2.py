6bac922978a3945dc5174f5188278b68149d4e1b
Aditya vardhan sharma <95498023+Adityaa-04@users.noreply.github.com>
2026-01-04 20:11:11 +0530

    Create new2.py

---
+++ new2.py
@@ -0,0 +1,22 @@
+with st.form(key="chat_form", clear_on_submit=True):
+
+    col1, col2 = st.columns([5, 1])
+
+    with col1:
+        user_input = st.text_input(
+            "Type your message...",
+            label_visibility="collapsed"
+        )
+
+    with col2:
+        audio = mic_recorder(
+            start_prompt="🎤",
+            stop_prompt="⏹",
+            key="mic"
+        )
+
+    send_button = st.form_submit_button(
+        "Send",
+        use_container_width=True,
+        type="primary"
+    )
